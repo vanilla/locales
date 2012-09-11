f9f519554c7a9cdadc7a0d04ba3ec5d5565544b6
Todd Burry <todd@vanillaforums.com>
2012-09-11 15:40:20 -0400

    Copy locales from addons repo.

---
+++ vf_fi_FL/archive_core.php
@@ -0,0 +1,7 @@
+<?php
+
+$Definition['GenderSuffix.First.f'] = '&nbsp;';
+$Definition['GenderSuffix.First.m'] = '&nbsp;';
+$Definition['GenderSuffix.Third.f'] = '&nbsp;';
+$Definition['GenderSuffix.Third.m'] = '&nbsp;';
+$Definition['GenderSuffix.Third.u'] = ' ';
---
+++ vf_hu_HU/definitions.php
@@ -0,0 +1,10 @@
+<?php
+
+ $LocaleInfo['vf_hu_HU'] = array (
+  'Locale' => 'hu-HU',
+  'Name' => 'Hungarian Transifex',
+  'Description' => 'Hungarian language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/hu/">here</a>.',
+  'Version' => '2012.09.11p1532',
+  'Author' => 'Vanilla Community',
+  'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/hu/',
+);
