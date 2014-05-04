c93d3e22e60bd9fc45e2038d9854a69101d1857b
Todd Burry <todd@vanillaforums.com>
2014-05-04 14:04:06 -0400

    Added the ru@petr1708 locale.

---
+++ vf_ru__PETR1708/archive_core.php
@@ -0,0 +1,7 @@
+<?php
+
+$Definition['GenderSuffix.First.f'] = 'СуффиксъПола.Первый.ж';
+$Definition['GenderSuffix.First.m'] = 'СуффиксъПола.Первый.м';
+$Definition['GenderSuffix.Third.f'] = 'СуффиксъПола.Третій.ж';
+$Definition['GenderSuffix.Third.m'] = 'СуффиксъПола.Третій.м';
+$Definition['GenderSuffix.Third.u'] = 'СуффиксъПола.Третій.с';
---
+++ vf_ru__PETR1708/definitions.php
@@ -0,0 +1,10 @@
+<?php
+
+ $LocaleInfo['vf_ru__PETR1708'] = array (
+  'Locale' => 'ru@petr1708',
+  'Name' => 'Russian (PETR1708) Transifex',
+  'Description' => 'Russian (PETR1708) language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/ru%40petr1708/">here</a>.',
+  'Version' => '2014.05.04p1400',
+  'Author' => 'Vanilla Community',
+  'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/ru%40petr1708/',
+);
