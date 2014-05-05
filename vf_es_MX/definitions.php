f15f9670a6df6af01474e6a455143bcf014d4fb1
Todd Burry <todd@vanillaforums.com>
2014-05-05 12:19:10 -0400

    Spanish (Mexico) locale.

---
+++ vf_es_MX/definitions.php
@@ -0,0 +1,10 @@
+<?php
+
+ $LocaleInfo['vf_es_MX'] = array (
+  'Locale' => 'es_MX',
+  'Name' => 'Spanish (Mexico) Transifex',
+  'Description' => 'Spanish (Mexico) language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/es_MX/">here</a>.',
+  'Version' => '2014.05.05p1217',
+  'Author' => 'Vanilla Community',
+  'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/es_MX/',
+);
