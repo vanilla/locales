60dec144bf89614db59c04e28b37cc74d7f66103
Todd Burry <todd@vanillaforums.com>
2014-08-12 14:17:27 -0400

    Add Tagalog locale.

---
+++ vf_tl/dash_core.php
@@ -0,0 +1,24 @@
+<?php
+
+$Definition['Add Info &amp; Create Account'] = 'Magdagdag ng Info &amp; Gumawa ng Account';
+$Definition['Applied On'] = 'petsa';
+
+$Definition['Badge Name'] = 'Pangalan';
+$Definition['Ban Item'] = 'Gamit';
+$Definition['Banner Title'] = 'Titulo ng Baner';
+$Definition['Ban Type'] = 'I-type';
+
+$Definition['Condition Expression'] = 'Halaga';
+$Definition['Condition Type'] = 'I-type';
+
+$Definition['Operation By'] = 'Ni';
+
+$Definition['Permission.Category'] = 'kategorya';
+
+$Definition['Saved'] = 'Ang iyong mga pagbabago ay nai-save.';
+$Definition['SaveLocalePack'] = 'I-save';
+
+$Definition['UserKeep'] = 'Panatilihin Ang Mga Nilalaman ng Gumagamit';
+$Definition['Use these fields to create custom profile information. You can enter things like "Relationship Status", "Skype", or "Favorite Dinosaur". Be creative!'] = 'Gamitin ang mga patlang na ito upang lumikha ng sinadya na impormasyon ng profile. Maaari kang magpasok ng mga bagay tulad ng "Katayuan ng Relasyon", "Skype", o "Mga Paboritong Dinosaur". Maging malikhain!';
+
+$Definition['Warning'] = 'Babala';
---
+++ vf_tl/definitions.php
@@ -0,0 +1,10 @@
+<?php
+
+ $LocaleInfo['vf_tl'] = array (
+  'Locale' => 'tl',
+  'Name' => 'Tagalog Transifex',
+  'Description' => 'Tagalog language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/tl/">here</a>.',
+  'Version' => '2014.08.12p1416',
+  'Author' => 'Vanilla Community',
+  'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/tl/',
+);
