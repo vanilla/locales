2f6888c859f8b0b60ea7dae66eb25fa4c5ffc4b7
Todd Burry <todd@vanillaforums.com>
2014-08-20 17:53:10 -0400

    Updates from Transifex.

--- vf_fr_FR/definitions.php
+++ vf_fr_FR/definitions.php
@@ -4,7 +4,7 @@
   'Locale' => 'fr_FR',
   'Name' => 'French (France) Transifex',
   'Description' => 'French (France) language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/fr_FR/">here</a>.',
-  'Version' => '2014.08.13p1144',
+  'Version' => '2014.08.20p1749',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/fr_FR/',
 );
