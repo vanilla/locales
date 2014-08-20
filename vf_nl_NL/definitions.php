dc94f50fdfda0418035a233d9e351da1c8b1e57c
Todd Burry <todd@vanillaforums.com>
2014-08-20 11:41:52 -0400

    Deprecate the old Dutch transifex.

--- vf_nl_NL/definitions.php
+++ vf_nl_NL/definitions.php
@@ -2,7 +2,7 @@
 
  $LocaleInfo['vf_nl_NL'] = array (
   'Locale' => 'nl-NL',
-  'Name' => 'Dutch Transifex',
+  'Name' => 'Dutch Transifex (Deprecated)',
   'Description' => 'Dutch language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/nl/">here</a>.',
   'Version' => '2012.10.05p1534',
   'Author' => 'Vanilla Community',
