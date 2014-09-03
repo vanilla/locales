dceb61fdd5b621e5580d0703682b48460ef0878c
Todd Burry <todd@vanillaforums.com>
2014-09-03 10:19:24 -0400

    Updates from transifex.

--- vf_de_DE/definitions.php
+++ vf_de_DE/definitions.php
@@ -4,7 +4,7 @@
   'Locale' => 'de_DE',
   'Name' => 'German (Germany) Transifex',
   'Description' => 'German (Germany) language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/de_DE/">here</a>.',
-  'Version' => '2014.08.20p1749',
+  'Version' => '2014.08.21p1057',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/de_DE/',
 );
--- vf_ru_RU/definitions.php
+++ vf_ru_RU/definitions.php
@@ -4,7 +4,7 @@
   'Locale' => 'ru_RU',
   'Name' => 'Russian (Russia) Transifex',
   'Description' => 'Russian (Russia) language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/ru_RU/">here</a>.',
-  'Version' => '2014.08.13p1144',
+  'Version' => '2014.08.20p1933',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/ru_RU/',
 );
