2bd292dc5705ac1a46ad6ad578dc9a9c08b66339
John Ashton <john.robert.ashton@gmail.com>
2014-09-22 18:55:17 -0400

    Deprecate: vf_bs_VA and vf_it_IT

--- vf_bs_BA/definitions.php
+++ vf_bs_BA/definitions.php
@@ -2,7 +2,7 @@
 
  $LocaleInfo['vf_bs_BA'] = array (
   'Locale' => 'bs-BA',
-  'Name' => 'Bosnian Transifex',
+  'Name' => 'Bosnian Transifex (Deprecated)',
   'Description' => 'Bosnian language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/bs/">here</a>.',
   'Version' => '2012.10.05p1534',
   'Author' => 'Vanilla Community',
