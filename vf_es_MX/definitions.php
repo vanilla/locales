3aaf267b3d94c62da3e7c94b07cf8feb3a280502
Todd Burry <todd@vanillaforums.com>
2014-10-07 12:01:25 -0400

    Update translations from transifex.

--- vf_es_MX/definitions.php
+++ vf_es_MX/definitions.php
@@ -4,7 +4,7 @@
   'Locale' => 'es_MX',
   'Name' => 'Spanish (Mexico) Transifex',
   'Description' => 'Spanish (Mexico) language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/es_MX/">here</a>.',
-  'Version' => '2014.10.06p2205',
+  'Version' => '2014.10.07p0032',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/es_MX/',
 );
--- vf_pl/definitions.php
+++ vf_pl/definitions.php
@@ -4,7 +4,7 @@
   'Locale' => 'pl',
   'Name' => 'Polish Transifex',
   'Description' => 'Polish language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/pl/">here</a>.',
-  'Version' => '2014.10.05p2341',
+  'Version' => '2014.10.07p0831',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/pl/',
 );
