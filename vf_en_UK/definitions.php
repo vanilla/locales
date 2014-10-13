652d113e963c38aa881f8eed3f533f2ae5013a0a
Todd Burry <todd@vanillaforums.com>
2014-10-13 09:46:27 -0400

    Remove the en_UK stub locale.
    This can be re-added if really necessary, but should be a proper transifex locale if so.

--- vf_en_UK/definitions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
- $LocaleInfo['vf_en_UK'] = array (
-  'Locale' => 'en_UK',
-  'Name' => 'English (United Kingdom)',
-  'Description' => 'English (United Kingdom) placedholder locale.',
-  'Version' => '2014.10.12',
-  'Author' => 'Vanilla Community'
-);
