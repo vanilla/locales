705754c221ba52ca14a7b3068ce43168e2fccc21
Todd Burry <todd@vanillaforums.com>
2014-11-06 08:52:42 -0500

    Add the French (Canada) locale.

---
+++ vf_fr_CA/definitions.php
@@ -0,0 +1,10 @@
+<?php
+
+ $LocaleInfo['vf_fr_CA'] = array (
+  'Locale' => 'fr_CA',
+  'Name' => 'Français (Canada) / French (Canada)',
+  'Description' => 'Official French (Canada) language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/fr_CA/">here</a>.',
+  'Version' => '2014.11.06p0850',
+  'Author' => 'Vanilla Community',
+  'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/fr_CA/',
+);
