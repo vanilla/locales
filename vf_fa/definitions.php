807f8a27b189e13d413d24ae62f5f083c49d6db8
Todd Burry <todd@vanillaforums.com>
2014-11-13 11:44:59 -0500

    Update translations from transifex.

--- vf_fa/definitions.php
+++ vf_fa/definitions.php
@@ -4,7 +4,7 @@
   'Locale' => 'fa',
   'Name' => 'فارسی / Persian',
   'Description' => 'Official Persian language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/fa/">here</a>.',
-  'Version' => '2014.11.07p0948',
+  'Version' => '2014.11.13p1136',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/fa/',
 );
