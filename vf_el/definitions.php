a1cc84616c98b6df311796a9a22b52717484cda9
Todd Burry <todd@vanillaforums.com>
2014-11-27 15:49:48 -0500

    Update translations from transifex.

--- vf_el/definitions.php
+++ vf_el/definitions.php
@@ -4,7 +4,7 @@
   'Locale' => 'el',
   'Name' => 'Ελληνικά / Greek',
   'Description' => 'Official Greek language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/el/">here</a>.',
-  'Version' => '2014.11.26p1210',
+  'Version' => '2014.11.27p1548',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/el/',
 );
