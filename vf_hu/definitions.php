4a88bbde802710e66f66b8f3af1d44a75cd5ef3a
Todd Burry <todd@vanillaforums.com>
2014-12-01 15:29:32 -0500

    Update translations from transifex.

--- vf_hu/definitions.php
+++ vf_hu/definitions.php
@@ -4,7 +4,7 @@
   'Locale' => 'hu',
   'Name' => 'Magyar / Hungarian',
   'Description' => 'Official Hungarian language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/hu/">here</a>.',
-  'Version' => '2014.11.26p1204',
+  'Version' => '2014.12.01p1527',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/hu/',
 );
