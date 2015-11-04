915cb705f1c36b49d5d16d8915efc56da96d732f
Todd Burry <todd@vanillaforums.com>
2015-11-04 15:18:06 -0500

    Update translations from Transifex

--- vf_zh/definitions.php
+++ vf_zh/definitions.php
@@ -4,7 +4,7 @@
   'Locale' => 'zh',
   'Name' => '中文 / Chinese',
   'Description' => 'Official Chinese language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/zh/">here</a>.',
-  'Version' => '2015.10.28p1301',
+  'Version' => '2015.11.04p1301',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/zh/',
 );
