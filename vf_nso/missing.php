71cfbbc5e045aebeb2f249c6616bd7c412d9e392
Todd Burry <todd@vanillaforums.com>
2016-02-03 14:27:25 -0500

    Update translations from Transifex

--- vf_nso/missing.php
+++ vf_nso/missing.php
@@ -1,64 +1,11 @@
 <?php
 
-$Definition['BanReason.2'] = 'Banned by IP address, email, or name.';
-$Definition['BanReason.4'] = 'Temporarily banned by a community manager.';
-$Definition['BanReason.8'] = 'Banned by warnings.';
-
-$Definition['CategoriesShowFollowed'] = 'Only show followed categories';
-$Definition['CategoriesShowUnfollowed'] = 'Show hidden categories';
-$Definition['CategoriesViewingAll'] = 'You are currently viewing all categories.';
-$Definition['CategoriesViewingFollowed'] = 'You are currently only viewing categories that you follow.';
-$Definition['Comment in'] = 'in';
-$Definition['Commenting as %1$s (%2$s)'] = 'Commenting as %1$s <span class="SignOutWrap">(%2$s)</span>';
-$Definition['ConnectAccountExists'] = 'You already have an account here.';
-$Definition['ConnectChooseName'] = 'Choose a name to identify yourself on the site.';
-$Definition['ConnectExistingPassword'] = 'Enter your existing account password.';
-$Definition['ConnectLeaveBlank'] = 'Leave blank unless connecting to an exising account.';
-$Definition['ConnectName'] = 'Username';
-$Definition['ConnectRegisteredName'] = 'Your registered username: <strong>%s</strong>';
-$Definition['Connect your profile to social networks.'] = 'Connect your profile to social networks to be notified of activity here and share your activity with your friends and followers.';
-
-$Definition['Date.DefaultDateTimeFormat'] = '%B %e, %Y %l:%M%p';
-$Definition['Date.DefaultDayFormat'] = '%B %e';
-$Definition['Date.DefaultFormat'] = '%B %e, %Y';
-$Definition['Date.DefaultTimeFormat'] = '%l:%M%p';
-$Definition['Date.DefaultYearFormat'] = '%B %Y';
-$Definition['Date Examples'] = 'Examples: Monday, today, last week, Mar 26, 3/26/04';
-$Definition['Define Thumbnail'] = 'Click and drag across the picture to define your thumbnail.';
-$Definition['Deny Applicant'] = 'Deny';
-
-$Definition['editor.BBCodeHelpText'] = 'You can use <a href="http://en.wikipedia.org/wiki/BBCode" target="_new">BBCode</a> in your post.';
-$Definition['editor.HtmlHelpText'] = 'You can use <a href="http://htmlguide.drgrog.com/cheatsheet.php" target="_new">Simple HTML</a> in your post.';
-$Definition['editor.MarkdownHelpText'] = 'You can use <a href="http://en.wikipedia.org/wiki/Markdown" target="_new">Markdown</a> in your post.';
-$Definition['editor.TextHelpText'] = 'You are using plain text in your post.';
-$Definition['editor.WysiwygHelpText'] = 'You are using <a href="https://en.wikipedia.org/wiki/WYSIWYG" target="_new">WYSIWYG</a> in your post.';
 $Definition['EmailConfirmEmail'] = 'You need to confirm your email address before you can continue. Please confirm your email address by clicking on the following link: {/entry/emailconfirm,exurl,domain}/{User.UserID,rawurlencode}/{EmailKey,rawurlencode}';
-$Definition['EmailFooter'] = '
-Have a great day!';
-$Definition['EmailHeader'] = 'Hello {User.Name}!
-';
-$Definition['EmailInvitation'] = 'Hello!
-
-%1$s has invited you to join %2$s. If you want to join, you can do so by clicking this link:
-
-  %3$s';
-$Definition['EmailMembershipApproved'] = 'Hello %1$s,
-
-You have been approved for membership. Sign in now at the following link:
-
-  %2$s';
-$Definition['EmailNotification'] = '%1$s
-
-Follow the link below to check it out:
-%2$s
-
-Have a great day!';
 $Definition['EmailPassword'] = '%2$s has reset your password at %3$s. Your login credentials are now:
 
   Email: %6$s
   Password: %5$s
   Url: %4$s';
-$Definition['Email Source'] = 'Email';
 $Definition['EmailStoryNotification'] = '%1$s
 
 %3$s
@@ -134,63 +81,10 @@ $Definition['Member Count'] = 'Members';
 $Definition['MyBadgesModuleTitle'] = 'My Badges';
 
 $Definition['new discussion'] = 'new';
-$Definition['NoBadgesEarned'] = 'Any minute now&hellip;';
-$Definition['NoPermissionToDeleteDiscussions'] = 'You do not have permission to delete %1$s of the selected discussions.';
-$Definition['Null Date'] = '-';
 
-$Definition['OldPassword'] = 'Old password';
-
-$Definition['ParticipatedHomepageTitle'] = 'Participated Discussions';
-$Definition['Password Contains Username'] = 'Contains Username';
-$Definition['Password Good'] = 'Good';
-$Definition['Password Ok'] = 'Ok';
-$Definition['PasswordRequest'] = 'Someone has requested to reset your password at %2$s. To reset your password, follow this link:
-
-  %3$s
-
-If you did not make this request, disregard this email.';
-$Definition['Password Strong'] = 'Strong';
-$Definition['Password Too Short'] = 'Too Short';
-$Definition['Password Very Weak'] = 'Very Weak';
-$Definition['Password Weak'] = 'Weak';
-$Definition['PermissionErrorMessage'] = 'You don\'t have permission to do that.';
-$Definition['PermissionErrorTitle'] = 'Permission Problem';
-$Definition['PermissionRequired.Garden.Moderation.Manage'] = 'You need to be a moderator to do that.';
-$Definition['PermissionRequired.Garden.Settings.Manage'] = 'You need to be an administrator to do that.';
-$Definition['PermissionRequired.Javascript'] = 'You need to enable javascript to do that.';
-$Definition['Please join my group.'] = 'Please join my <a href="{Url,html}">group</a>.';
-$Definition['Posts.Plural: %s'] = 'Posts: %s';
-$Definition['Posts.Singular: %s'] = 'Posts: %s';
-$Definition['PreferenceBadgeEmail'] = 'Notify me when I earn a badge.';
-$Definition['PreferenceBadgePopup'] = 'Notify me when I earn a badge.';
-$Definition['Private group.'] = 'Anyone can see the group, but only members can see its content. People must apply or be invited to join.';
-$Definition['ProfileFieldsCustomDescription'] = 'Use these fields to create custom profile information. You can enter things like "Relationship Status", "Skype", or "Favorite Dinosaur". Be creative!';
-$Definition['Public group.'] = 'Anyone can see the group and its content. Anyone can join.';
-
-$Definition['Q&A Accepted'] = 'Answered ✓';
-$Definition['Q&A Answered'] = 'Answered';
-$Definition['Q&A Question'] = 'Question';
-$Definition['QnA Accepted Answer'] = 'Answer ✓';
-$Definition['QnA Rejected Answer'] = 'Rejected Answer';
-$Definition['Quote on'] = 'on';
-$Definition['Quote wrote'] = 'wrote';
-
-$Definition['Ranks.ActivityFormat'] = '{ActivityUserID,user} {ActivityUserID,plural,was,were} promoted to {Data.Name,plaintext}.';
 $Definition['Ranks.NotificationFormat'] = 'Congratulations! You\'ve been promoted to {Data.Name,plaintext}.';
 $Definition['RecipientUserID'] = 'recipient';
-$Definition['ReplyOnly'] = 'You can reply to this email directly to follow up. Sent from {/,exurl}.';
-$Definition['ReplyOrFollow'] = 'Reply to this email directly or follow the link below to check it out:';
-$Definition['Report Reason'] = 'Reason';
-$Definition['RoleID'] = 'role';
-$Definition['RSVP'] = 'Respond';
 
-$Definition['SearchBoxPlaceHolder'] = 'Search';
-$Definition['Search for a tag.'] = 'Search for all or part of a tag.';
-$Definition['sep and'] = 'and';
-$Definition['Sign In or Register to Comment.'] = '<a href="{SignInUrl,html}"{Popup}>Sign In</a> or <a href="{RegisterUrl,html}">Register</a> to comment.';
-
-$Definition['TagHint'] = 'Start to type...';
-$Definition['TermsOfService'] = 'Terms of Service';
 $Definition['TermsOfServiceText'] = 'You agree, through your use of this service, that you will not use this
 community to post any material which is knowingly false and/or defamatory,
 inaccurate, abusive, vulgar, hateful, harassing, obscene, profane, sexually
@@ -211,9 +105,6 @@ all. We reserve the right to terminate any membership for any reason or no
 reason at all.
 
 You must be at least 13 years of age to use this service.';
-$Definition['The quote had to be converted from %s to %s.'] = 'The quote had to be converted from %s to %s. Some formatting may have been lost.';
-$Definition['There\'s already a %s with the name %s.'] = 'There\'s already a %1$s with the name %2$s.';
-$Definition['The user isn\'t banned.'] = 'The user isn\'t banned or is banned by some other function.';
 
 $Definition['Unanswered Questions'] = 'Unanswered';
 $Definition['UrlCode'] = 'Url code';
