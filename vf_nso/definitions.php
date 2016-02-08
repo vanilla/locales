e7625788072a96de35ee0489e0fe77d963619f65
Todd Burry <todd@vanillaforums.com>
2016-02-08 13:53:28 -0500

    Update the naming for Zulu and Northern Sotho
    Hat tip to Oxford press.

--- vf_nso/definitions.php
+++ vf_nso/definitions.php
@@ -2,7 +2,7 @@
 
  $LocaleInfo['vf_nso'] = array (
   'Locale' => 'nso',
-  'Name' => 'Northern Sotho',
+  'Name' => 'Sesotho sa Leboa / Northern Sotho',
   'Description' => 'Official Northern Sotho language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/nso/">here</a>.',
   'Version' => '2016.02.04p0915',
   'Author' => 'Vanilla Community',
--- vf_zu_ZA/definitions.php
+++ vf_zu_ZA/definitions.php
@@ -2,7 +2,7 @@
 
  $LocaleInfo['vf_zu_ZA'] = array (
   'Locale' => 'zu_ZA',
-  'Name' => 'IsiZulu (i-South Africa) / Zulu (South Africa)',
+  'Name' => 'isiZulu / Zulu',
   'Description' => 'Official Zulu (South Africa) language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/zu_ZA/">here</a>.',
   'Version' => '2016.02.04p1601',
   'Author' => 'Vanilla Community',
