83a264156f083459cf47c3b9592daca7b516f60b
Todd Burry <todd@vanillaforums.com>
2016-02-29 10:15:17 -0500

    Update translations from Transifex

--- vf_ur/definitions.php
+++ vf_ur/definitions.php
@@ -4,7 +4,7 @@
   'Locale' => 'ur',
   'Name' => 'اردو / Urdu',
   'Description' => 'Official Urdu language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/ur/">here</a>.',
-  'Version' => '2016.02.27p0201',
+  'Version' => '2016.02.28p1301',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/ur/',
   'License' => 'CC BY-SA 4.0',
