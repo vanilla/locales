317786b049573c86f1f620da61d2a6e0a17647e0
Todd Burry <todd@vanillaforums.com>
2016-03-24 08:55:37 -0400

    Add translations from Transifex

--- vf_bg/definitions.php
+++ vf_bg/definitions.php
@@ -4,7 +4,7 @@
   'Locale' => 'bg',
   'Name' => 'Български / Bulgarian',
   'Description' => 'Official Bulgarian language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/bg/">here</a>.',
-  'Version' => '2016.03.23p1601',
+  'Version' => '2016.03.24p0801',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/bg/',
   'License' => 'CC BY-SA 4.0',
