eb616f367fb113cadc3c456046c0cc158c8e9dc9
Todd Burry <todd@vanillaforums.com>
2016-03-25 18:11:32 -0400

    Update translations from Transifex

--- vf_de/dash_text.php
+++ vf_de/dash_text.php
@@ -7,7 +7,7 @@ $Definition['About.DisableStatistics'] = 'Falls du die Datenübermittlung aus Un
 $Definition['About.VanillaStatistics'] = 'Es ist absolut notwendig für die Erhaltung dieser kostenlosen, open-source Software, dass wir akkurate Messungen zur Verbreitung und Effektivität von Vanilla erhalten. Daher bitten wir darum, die Datenübermittlung nicht auszuschalten.';
 $Definition['AboutCommentEmbedding'] = 'Vanilla kann als "drop-in" Ersatz für deine Standardkommentarfunktion deines Blogs genutzt werden. Es kann genutzt werden, um Kommentare auf jeder Webseite hinzuzufügen.';
 $Definition['AboutEmbedding'] = 'Embedding erlaubt es allen oder Teilen der Community, in andere Anwendungen und Webseiten integriert zu werden.';
-$Definition['About Vanilla Statistics'] = 'Über Vanilla Statistik';
+$Definition['About Vanilla Statistics'] = 'Über Vanilla-Statistik';
 $Definition['Access your community forum by clicking the "Visit Site" link.'] = 'Greifen Sie auf Ihr Community-Forum zu, indem Sie oben den Link „Seite besuchen“ anklicken oder <a href={/,url}>hier klicken</a>. Das Community-Forum ist das, was all Ihre Nutzer und Kunden sehen, wenn sie <a href="{/,url,domain}">{/,url,domain}</a> besuchen.';
 $Definition['AddonProblems'] = '<h2>Probleme?</h2><p>Wenn etwas mit einem Addon schief geht und Sie es Ihre Seite nicht verwenden können, können Sie das Addon manuell abschalten durch Bearbeitung:</p>%s ';
 $Definition['All categories listed with a selection of 5 recent discussions under each'] = 'Alle Kategorien werden gelistet mit den jeweils fünf neuesten Diskussionen darunter';
@@ -30,7 +30,7 @@ $Definition['Define who can upload files on the Roles & Permissions page.'] = 'L
 $Definition['Define your forum homepage, upload your logo, and more...'] = 'Wähle deine Foren-Homepage, lade dein Logo hoch und mehr...';
 $Definition['Deleting a role can result in users not having access to the application.'] = 'Eine Rolle zu löschen kann dazu führen, dass Benutzer keinen Zugang mehr zur Anwendung haben.';
 $Definition['Deleting forever cannot be undone.'] = 'Löschen ist eine gute Möglichkeit, das Forum sauber zu halten.';
-$Definition['Disable Google Sign In'] = 'Google Sign In deaktivieren';
+$Definition['Disable Google Sign In'] = 'Google Sign-In deaktivieren';
 $Definition['Disable OpenID'] = 'OpenID deaktivieren';
 $Definition['Discussion categories are used to help your users organize their discussions in a way that is meaningful for your community.'] = 'Diskussionskategorien sind sinnvoll, um deinen Benutzern zu helfen, Diskussionen in einer sinnvollen Art und Weise zu organisieren.';
 $Definition['Don\'t embed your forum admin dashboard (pop it out to full-screen)'] = 'Forum-Admin-Dashboard nicht einbetten (in neuem Fenster öffnen)';
@@ -43,14 +43,14 @@ $Definition['Enter a descriptive name.'] = 'Gib einen beschreibenden Namen für 
 $Definition['Enter a short name for the site.'] = 'Gib einen kurzen Namen für die Seite ein. Dies wird auf den Anmeldebuttons angezeigt.';
 $Definition['Enter the address of your Wordpress Blog and we\'ll take it from there.'] = 'Gib die Adressse deines Wordpress Blogs ein und wir kümmern uns um den Rest.';
 $Definition['Enter the connection settings for your sphinx server below.'] = 'Gib die Verbindungseinstellungen für deinen Sphinx Server unten ein.';
-$Definition['Every edit or deletion is recorded here. Use &lsquo;Restore&rsquo; to undo any change.'] = 'Jede Bearbeitung und Löschung wird hier aufgezeichnet. Benutze &lsquo;Wiederherstellen&rsquo;, um Änderungen rückgängig zu machen.';
+$Definition['Every edit or deletion is recorded here. Use &lsquo;Restore&rsquo; to undo any change.'] = 'Jede Bearbeitung und Löschung wird hier aufgezeichnet. Benutze ‚Wiederherstellen‘, um Änderungen rückgängig zu machen.';
 $Definition['Everyone who signs up gets a profile page.'] = 'Alle, die sich bei Ihrer Community anmelden, bekommen eine öffentliche Profilseite, wo sie ein Foto von sich hochladen, ihre Profileinstellungen verwalten und coole Dinge, die in der Community ablaufen, tracken können. Sie sollten <a href="{/profile,url}">jetzt Ihr Profil persönlich anpassen</a>.';
 $Definition['Every user in your site is assigned to at least one role. Roles are used to determine what the users are allowed to do.'] = 'Jeder Nutzer der Seite bekommt mindestens eine Rolle zugewiesen. Rollen bestimmen, was ein Nutzer auf der Seite machen darf.';
 
 $Definition['Facebook Connect allows users to sign in using their Facebook account.'] = 'Facebook Connect erlaubt Mitgliedern die Nutzung ihres Facebook Kontos. <b>Die Anwendung muss bei Facebook registriert sein, damit dies funktioniert.</b>';
 $Definition['Failed to connect to the database with the username and password you entered. Did you mistype them? The database reported: <code>%s</code>'] = 'Verbindung zur Datenbank mit dem angegebenen Benutzernamen und Passwort fehlgeschlagen. Hast du dich vertippt? Die Datenbank meldet: <code>%s</code>';
 $Definition['Force the dashboard to only be accessible through this url <em>(not recommended)</em>'] = 'Das Dashboard dazu zwingen, nur über diese URL erreichbar zu sein <em>(nicht empfohlen)</em>';
-$Definition['For SEO purposes, search engine crawlers are excluded from being forced to view the forum in the remote url.'] = 'Aus Gründen, die das SEO betreffen, werden Suchmaschinencrawler davin ausgeschlossen, das Forum über die Remote-URL aufzurufen.';
+$Definition['For SEO purposes, search engine crawlers are excluded from being forced to view the forum in the remote url.'] = 'Aus Gründen, die das SEO betreffen, werden Suchmaschinen-Crawler darin ausgeschlossen, das Forum über die Remote-URL aufzurufen.';
 
 $Definition['Garden.Import.Complete.Description'] = 'Sie haben einen Import erfolgreich beendet.
 Klicken Sie auf <b>Beendet</b>, wenn Sie fertig sind. ';
@@ -61,7 +61,7 @@ $Definition['Garden.StatisticsDisabled'] = 'Du hast die Vanilla-Statistiken ausd
 $Definition['Garden.StatisticsLocal.Explain'] = 'Dieses Forum scheint in einer Testumgebung zu laufen oder übermittelt anderweitig eine lokale IP-Adresse. Standardmäßig werden Foren, die lokal betrieben werden nicht verfolgt.';
 $Definition['Garden.StatisticsLocal.Resolve'] = 'Wenn du dir sicher bist, dass das Forum aus dem Internet verfügbar ist, kannst du die Aktivierung der Statistiken hier erzwingen:';
 $Definition['Garden.StatisticsReadonly.Explain'] = 'Deine cofig.php-Datei scheint nur Leserechte zu besitzen. Dies bedeutet, dass Vanilla nicht automatisch deine Installations-ID und Installations-Secret registrieren kann.';
-$Definition['Garden.StatisticsReadonly.Resolve'] = 'Um dieses Problem zu beheben, gib der Datei conf/config.php Chmod 777.';
+$Definition['Garden.StatisticsReadonly.Resolve'] = 'Um dieses Problem zu beheben, setze die Rechte der Datei conf/config.php per chmod auf 777.';
 
 $Definition['Heads Up! This is a special role that does not allow active sessions. For this reason, the permission options have been limited to "view" permissions.'] = 'Dies ist eine spezielle Rolle, die keine aktiven Sitzungen erlaubt. Daher sind die Rechteeinstellungen auf Rechte zur "Ansicht" eingeschränkt.';
 
@@ -78,7 +78,7 @@ $Definition['Kick-start your community and increase user engagement.'] = 'Starte
 $Definition['Learn about custom routing.'] = 'Weitere Informationen zu eigenen Routen.';
 $Definition['Learn how to completely change your forum\'s look and feel: upload your logo, set your homepage, choose a theme and customize it.'] = 'Lerne, wie du die komplette Erscheinung deines Forums verändern kannst: Lade dein Logo hoch, ändere deine Homepage, wähle ein Thema aus und passe es an.';
 $Definition['Learn how to use the basic functionality of your forum.'] = 'Lerne die Basisfunktionen deines Forums kennen.';
-$Definition['Locales allow you to support other languages on your site.'] = 'Locales gestatten Ihnen, auf Ihrer Seite andere Sprachen zu unterstützen. Aktivieren und deaktivieren Sie hier Locales, die Sie einstellen wollen.';
+$Definition['Locales allow you to support other languages on your site.'] = 'Lokalisierungen gestatten Ihnen, auf Ihrer Seite andere Sprachen zu unterstützen. Aktivieren und deaktivieren Sie hier Lokalisierungen, die Sie einstellen wollen.';
 $Definition['Locales are in your %s folder.'] = 'Lokalisierungen erlauben die Unterstützung unterschiedlicher Sprachen auf der Seite. Sobald eine Lokalisierung in den Ordner %s kopiert wurde, kann sie hier aktiviert werden.';
 $Definition['LogoDescription'] = 'Das Bannerlogo erscheint oben auf deiner Seite. Manche Themen zeigen es unter Umständen nicht an.';
 
@@ -100,9 +100,9 @@ $Definition['Quick-Start Guide to Creating Themes for Vanilla'] = 'Kurzanleitung
 
 $Definition['Ready-made Vanilla Comments Plugin for WordPress'] = 'Vergefertigtes Vanilla-Comment-Plugin für WordPress';
 $Definition['Ready-made Vanilla Forum Plugin for WordPress'] = 'Vergefertigtes Vanilla-Comment-Plugin für WordPress';
-$Definition['Realtime progress bars: %s'] = 'Echtzeit Ladebalken: %s';
+$Definition['Realtime progress bars: %s'] = 'Echtzeit-Ladebalken: %s';
 $Definition['Restoring your selection removes the items from this list.'] = 'Wenn du Elemente wiederherstellst, werden sie aus dieser Liste entfernt und wieder auf der Seite angezeigt.';
-$Definition['Route Expression'] = 'Routen Bezeichner';
+$Definition['Route Expression'] = 'Routen-Bezeichner';
 $Definition['Routes are used to redirect users.'] = 'Routen werden genutzt, um einen Seitenzugriff in Abhängigkeit der URL umzuleiten.';
 
 $Definition['Search for users by name or enter the name of a role to see all users with that role.'] = 'Suche nach Nutzern durch Eingabe des Benutzernamens oder gib den Namen einer Rolle an, um alle Nutzer zu sehen, denen diese Rolle zugewiesen wurde.';
@@ -117,15 +117,15 @@ $Definition['Start the first discussion.'] = 'Bringen Sie den Stein in Ihrer Com
 $Definition['The %s Authenticator does not have any custom configuration options.'] = 'Das %s Authentisierungs-Plugin hat keine weiteren Einstellungen.';
 $Definition['The addon could not be enabled because it generated a fatal error: <pre>%s</pre>'] = 'Das Addon konnte nicht aktiviert werden, weil es einen Ausnahmefehler verursachte: <pre>%s</pre>';
 $Definition['The addon has already been downloaded.'] = 'Das Addon wurde bereits heruntergeladen.';
-$Definition['The banner logo appears at the top of your forum.'] = 'Das banner logo erscheint oberhalb des Forums.';
-$Definition['The banner title appears on the top-left of every page.'] = 'Der Bannertitel erscheint in der oberen linken Ecke auf jeder Seite.';
-$Definition['The banner title appears on your site\'s banner and in your browser\'s title bar.'] = 'Der Bannertitel wird auf jeder Seite oben links angezeigt und in der Titelleiste deines Browsers. Er sollte weniger als 20 Zeichen haben. Wurde ein Logo hochgeladen, wird dies stattdessen auf allen Seiten angezeigt. Beachte auch, dass manche Themen diesen Titel verstecken.';
+$Definition['The banner logo appears at the top of your forum.'] = 'Das Banner-Logo erscheint oberhalb des Forums.';
+$Definition['The banner title appears on the top-left of every page.'] = 'Der Banner-Titel erscheint in der oberen linken Ecke auf jeder Seite.';
+$Definition['The banner title appears on your site\'s banner and in your browser\'s title bar.'] = 'Der Banner-Titel wird auf jeder Seite oben links angezeigt und in der Titelleiste deines Browsers. Er sollte weniger als 20 Zeichen haben. Wurde ein Logo hochgeladen, wird dies stattdessen auf allen Seiten angezeigt. Beachte auch, dass manche Themen diesen Titel verstecken.';
 $Definition['The basic registration form requires new users to copy text from a CAPTCHA image.'] = '<strong>Das einfache Anmeldeformular verlangt von</strong> neuen Usern, Text von einem CAPTCHA-Bild zu kopieren, um Spammer von der Seite fernzuhalten. Sie benötigen einen Account bei <a href="http://recaptcha.net/">recaptcha.net</a>. Die Anmeldung ist GRATIS und einfach. Sobald Sie sich angemeldet haben, kommen Sie hierher zurück und geben Sie die folgenden Einstellungen ein:';
 $Definition['The database user you specified does not have permission to access the database. Have you created the database yet? The database reported: <code>%s</code>'] = 'Der angegebene Datenbankbenutzer besitzt nicht die nötigen Berechtigungen, um auf die Datenbank zuzugreifen. Hast du die Datenbank bereits angelegt? Die Datenbank meldet: <code>%s</code>';
 $Definition['The following structure changes are required for your database.'] = 'Die folgenden Strukturänderungen sind notwendig in deiner Datenbank.';
 $Definition['The import file does not contain any data.'] = 'Die Import-Datei enthält keine Daten.';
 $Definition['The import file is not in the correct format.'] = 'Die Import-Datei ist nicht im korrekten Format.';
-$Definition['The location of the jsonp formatted authentication data.'] = 'Der Ort der jsonp-formatierten Authentifizierungsdaten.';
+$Definition['The location of the jsonp formatted authentication data.'] = 'Der Ort der JSONP-formatierten Authentifizierungsdaten.';
 $Definition['ThemeHelp'] = 'Themen erlauben es Ihnen, das Aussehen &amp; den Stil Ihrer Seite zu ändern.<br />Sobald ein Thema zu Ihrem %s Ordner hinzugefügt wurde, können Sie es hier aktivieren . ';
 $Definition['The Password Authenticator does not have any custom configuration options. '] = 'Der Passwort-Authenticator hat keine eigenen Einstellungsoptionen. ';
 $Definition['There are no database structure changes required. There may, however, be data changes.'] = 'Ist ist keine Änderung an der Datenbankstruktur nötig. Es könnten allerdings veränderte Daten vorliegen.';
@@ -143,8 +143,8 @@ $Definition['Twitter Connect allows users to sign in using their Twitter account
 
 $Definition['Use the plugin for WordPress or our universal code for any other platform'] = 'Benutze das WordPress Plugin, um Vanilla Kommentare auf deinem Blog zu verwenden; oder benutze den universellen Code, um Vanilla Kommentare auf jeder beliebigen Webseite einzubinden.';
 
-$Definition['Vanilla Export: %s, Source: %s'] = 'Vanilla Export: %s, Quelle: %s';
-$Definition['Vanilla Statistics are currently disabled'] = 'Vanilla Statistiken sind momentan deaktiviert';
+$Definition['Vanilla Export: %s, Source: %s'] = 'Vanilla-Export: %s, Quelle: %s';
+$Definition['Vanilla Statistics are currently disabled'] = 'Vanilla-Statistiken sind momentan deaktiviert';
 $Definition['Video tutorial on advanced settings'] = 'Videotutorial über erweitere Einstellungen';
 $Definition['Video tutorial on finding &amp; managing users'] = 'Videotutorial über das Suchen &amp; Verwalten von Benutzern';
 $Definition['Video tutorial on managing appearance'] = 'Videotutorial über das Verwalten &amp; Anpassen der Oberfläche';
