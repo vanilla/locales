4829cbd4c96ad86d5b744e26b853077463256945
Todd Burry <todd@vanillaforums.com>
2016-04-08 16:04:11 -0400

    Update translations from Transifex

--- vf_cy/definitions.php
+++ vf_cy/definitions.php
@@ -4,11 +4,12 @@
   'Locale' => 'cy',
   'Name' => 'Cymraeg / Welsh',
   'Description' => 'Official Welsh language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/cy/">here</a>.',
-  'Version' => '2016.04.08p1507',
+  'Version' => '2016.04.08p1601',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/cy/',
   'License' => 'CC BY-SA 4.0',
   'PercentComplete' => 6,
   'NumComplete' => 133,
   'DenComplete' => 2219,
+  'Icon' => 'cy.png',
 );
