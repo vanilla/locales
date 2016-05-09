d1fdea3523ab6d4c3f77b93919af32272e8f8177
Todd Burry <todd@vanillaforums.com>
2016-05-09 14:11:06 -0400

    Icon changes

--- vf_lt/definitions.php
+++ vf_lt/definitions.php
@@ -5,7 +5,7 @@
   'Name' => 'Lietuvių / Lithuanian',
   'EnName' => 'Lithuanian',
   'Description' => 'Official Lithuanian language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/lt/">here</a>.',
-  'Version' => '2016.05.08p2101',
+  'Version' => '2016.05.09p1230',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/lt/',
   'License' => 'CC BY-SA 4.0',
