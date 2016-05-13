130b98f629ebe3cfb5be68998c55adec91e3631f
Todd Burry <todd@vanillaforums.com>
2016-05-13 10:58:37 -0400

    Update translations from Transifex

--- vf_pl/definitions.php
+++ vf_pl/definitions.php
@@ -5,7 +5,7 @@
   'Name' => 'Polski / Polish',
   'EnName' => 'Polish',
   'Description' => 'Official Polish language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/pl/">here</a>.',
-  'Version' => '2016.05.09p1230',
+  'Version' => '2016.05.13p1031',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/pl/',
   'License' => 'CC BY-SA 4.0',
