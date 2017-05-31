10c3640cd99d44067548467c70ffa54f3e50c39d
Patrick Kelly <nervoustwit@users.noreply.github.com>
2017-05-31 14:45:38 -0400

    Add strings to translate next quote and previous quotes in comments.

--- tx-source/site_core.php
+++ tx-source/site_core.php
@@ -1,4 +1,6 @@
 <?php
+$Definition['&laquo; hide previous quotes'] = 'Hide Previous Quotes';
+$Definition['&raquo; show previous quotes'] = 'Show Previous Quotes';
 $Definition[' - Page %s'] = ' - Page %s';
 $Definition[' now.'] = ' now.';
 $Definition['%1$s accepted %4$s invitation for membership.'] = '%1$s accepted %4$s invitation for membership.';
