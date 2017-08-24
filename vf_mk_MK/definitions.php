e9acbc136cf2638bc2dfe713ba18fee853383e98
initvector <ryan.p@vanillaforums.com>
2017-08-24 16:52:57 -0400

    Update translations from Transifex

--- vf_mk_MK/definitions.php
+++ vf_mk_MK/definitions.php
@@ -5,11 +5,11 @@
   'Name' => 'Македонски (Македонија) / Macedonian (Macedonia)',
   'EnName' => 'Macedonian (Macedonia)',
   'Description' => 'Official Macedonian (Macedonia) language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/mk_MK/">here</a>.',
-  'Version' => '2017.08.23p1720',
+  'Version' => '2017.08.24p1639',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/mk_MK/',
   'License' => 'CC BY-SA 4.0',
   'PercentComplete' => 12,
   'NumComplete' => 269,
-  'DenComplete' => 2302,
+  'DenComplete' => 2303,
 );
