a366a9ac78f8da924f231c013971b7e3e94c71d4
Todd Burry <todd@vanillaforums.com>
2018-01-22 10:10:39 -0500

    Remove the vf_my_MM locale
    The translations are maintained at the “my” locale so this one is extraneous.

--- vf_my_MM/dash_core.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$Definition['Administrator'] = 'စီမံခန့်ခွဲသူ';
-
-$Definition['Permission.Category'] = 'အမျိုးအစား';
-
-$Definition['SaveLocalePack'] = 'သိမ်းရန်';
-
-$Definition['Why do you want to join?'] = 'သင်ဘာကြောင့် ချိတ်ဆက်ချင်တာလဲ ?';
