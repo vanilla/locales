ef414b9350e67711aed2b1f52b44b9e9fb9394c7
Todd Burry <todd@vanillaforums.com>
2018-03-28 09:18:26 -0400

    Update translations from Transifex and add PNGs back to the manifests

--- vf_oc/definitions.php
+++ vf_oc/definitions.php
@@ -5,11 +5,11 @@
   'Name' => 'Occitan',
   'EnName' => 'Occitan',
   'Description' => 'Official Occitan language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/oc/">here</a>.',
-  'Version' => '2018.03.21p1002',
+  'Version' => '2018.03.28p0904',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/oc/',
   'License' => 'CC BY-SA 4.0',
   'PercentComplete' => 0,
   'NumComplete' => 0,
-  'DenComplete' => 2320,
+  'DenComplete' => 2325,
 );
