75e42a4e313c4e4224bc9f5316af668280d1ad7e
Adam Charron <adam@charrondev.com>
2018-07-30 12:20:13 -0400

    Add extra source strings

--- tx-source/dash_core.php
+++ tx-source/dash_core.php
@@ -679,8 +679,12 @@ $Definition['Your invitations were sent successfully.'] = 'Your invitations were
 $Definition['InputFormatter.Notes1'] = 'Select the default format of the editor for posts in the community.';
 $Definition['InputFormatter.Notes2'] = 'The editor will auto-detect the format of old posts when editing them and load their
 original formatting rules. Aside from this exception, the selected post format below will take precedence.';
+$Definition['ForceWysiwyg.Notes1'] = 'Check the below option to tell the editor to reinterpret all old posts as Wysiwyg.';
 $Definition['ForceWysiwyg.Notes2'] = 'This setting will only take effect if Wysiwyg was chosen as the Post Format above. The purpose of this option is to normalize the editor format. If older posts edited with another format, such as markdown or BBCode, are loaded, this option will force Wysiwyg.';
 $Definition['MobileInputFormatter.Notes1'] = 'Specify an editing format for mobile devices.';
-$Definition['MobileInputFormatter.Notes'] = 'If mobile devices should have the same experience,
+$Definition['MobileInputFormatter.Notes2'] = 'If mobile devices should have the same experience,
 specify the same one as above. If users report issues with mobile editing, this is a good option to change.';
 $Definition['Note:'] = 'Note:';
+$Definition['Post Format'] = 'Post Format';
+$Definition['Reinterpret All Posts As Wysiwyg'] = 'Reinterpret All Posts As Wysiwyg';
+$Definition['Mobile Format'] = 'Mobile Format';
