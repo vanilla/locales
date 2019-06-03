4a8db894a1f58160d8a84b23df1bab5e2563ce4f
Todd Burry <todd@vanillaforums.com>
2019-06-03 11:24:49 -0400

    Revert "Delete deprecated vf_cs_CZ"
    This reverts commit 87beeaa017124d0b72ca805b491043797d739e90.

---
+++ vf_zh/missing.php
@@ -0,0 +1,12 @@
+<?php
+
+$Definition['ForceWysiwyg.Notes1'] = 'Check the below option to tell the editor to reinterpret all old posts as Wysiwyg.';
+$Definition['ForceWysiwyg.Notes2'] = 'This setting will only take effect if Wysiwyg was chosen as the Post Format above. The purpose of this option is to normalize the editor format. If older posts edited with another format, such as markdown or BBCode, are loaded, this option will force Wysiwyg.';
+
+$Definition['InputFormatter.Notes1'] = 'Select the default format of the editor for posts in the community.';
+$Definition['InputFormatter.Notes2'] = 'The editor will auto-detect the format of old posts when editing them and load their original formatting rules. Aside from this exception, the selected post format below will take precedence.';
+
+$Definition['MobileInputFormatter.Notes1'] = 'Specify an editing format for mobile devices.';
+$Definition['MobileInputFormatter.Notes2'] = 'If mobile devices should have the same experience, specify the same one as above. If users report issues with mobile editing, this is a good option to change.';
+
+$Definition['RichEditor.QuoteEnable.Notes'] = 'Use the following option to enable quotes for the Rich Editor. This will only apply if the default formatter is "Rich".';
