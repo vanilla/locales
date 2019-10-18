ea093fa97e206a1fea8e46d97950805dd99d32c0
Todd Burry <todd@vanillaforums.com>
2019-10-18 00:51:17 -0400

    Update translations from Transifex

---
+++ vf_de/site_kb.php
@@ -0,0 +1,137 @@
+<?php
+
+$Definition["(No Body)"] = "(Kein Haupttext)";
+$Definition["(Untitled)"] = "(ohne Titel)";
+$Definition["<0 /> out of <1 /> people found this helpful"] = "<0 /> von <1 /> Personen fanden dies hilfreich";
+
+$Definition["Add Knowledge Base"] = "Wissensdatenbank hinzufügen";
+$Definition["Add a knowledge base to get started."] = "Fügen Sie eine Wissensdatenbank hinzu, um zu beginnen.";
+$Definition["Add/Edit Knowledge Base"] = "Wissensdatenbank hinzufügen/bearbeiten";
+$Definition["Admin Links"] = "Admin-Links";
+$Definition["Are you sure you want to delete <0/> \"<1/>\" ?"] = "Sind Sie sicher, dass Sie <0/> \"<1/>\" löschen möchten?";
+$Definition["Are you sure you want to delete this knowledge base?"] = "Sind Sie sicher, dass Sie diese Wissensdatenbank löschen möchten?";
+$Definition["Are you sure you want to purge this knowledge base?"] = "Sind Sie sicher, dass Sie diese Wissensdatenbank bereinigen möchten?";
+$Definition["Are you sure you want to restore this knowledge base?"] = "Sind Sie sicher, dass Sie diese Wissensdatenbank wiederherstellen möchten?";
+$Definition["Article"] = "Artikel";
+$Definition["article"] = "Artikel";
+$Definition["Article One"] = "Erster Artikel";
+$Definition["Article Options"] = "Artikeloptionen";
+$Definition["Article Revisions"] = "Artikelüberarbeitungen";
+$Definition["Article Three"] = "Artikel Drei";
+$Definition["Article Two"] = "Zweiter Artikel";
+$Definition["Articles"] = "Artikel";
+
+$Definition["Back to Home"] = "Zurück zum Start";
+$Definition["Back to home page"] = "Zurück zur Startseite";
+$Definition["Be the first one to vote!"] = "Seien Sie der Erste, der abstimmt!";
+$Definition["Browse Articles by Category"] = "Artikel nach Kategorien durchsuchen";
+$Definition["By <0/>"] = "Durch <0/>";
+
+$Definition["category"] = "Kategorie";
+$Definition["Category One"] = "Erste Kategorie";
+$Definition["Category Options"] = "Kategorien-Optionen";
+$Definition["Category Two"] = "Zweite Kategorie";
+$Definition["Choose"] = "Wählen";
+$Definition["Choose a location for this page."] = "Wählen Sie eine Position für diese Seite.";
+$Definition["Choose a subcommunity"] = "Wählen Sie eine Subcommunity aus";
+$Definition["Compose"] = "Zusammenstellen";
+$Definition["Contents of folder: "] = "Ordnerinhalt:";
+$Definition["Convert to Article"] = "In Artikel umsetzen";
+$Definition["Create Article"] = "Artikel erstellen";
+$Definition["Create new article"] = "Neuen Artikel erstellen";
+$Definition["Create one to get started!"] = "Erstellen Sie einen, um zu beginnen!";
+$Definition["Customize SEO"] = "Suchmaschinen-Optimierung anpassen";
+$Definition["Debug"] = "Debuggen";
+
+$Definition["Delete \"<0/>\""] = "\"<0/>\" löschen";
+$Definition["Delete Draft"] = "Entwurf löschen";
+$Definition["Delete an Article"] = "Einen Artikel löschen";
+$Definition["Deleted Articles"] = "Gelöschte Artikel";
+$Definition["Draft Options"] = "Entwurfs-Optionen";
+$Definition["Draft saved <0/>"] = "Entwurf gespeichert <0/>";
+
+$Definition["Error saving draft."] = "Fehler beim Speichern des Entwurfs.";
+$Definition["Everywhere"] = "Überall";
+$Definition["Example: Appearance"] = "Beispiel: Aufmachung";
+
+$Definition["Filter"] = "Filter";
+$Definition["Filter Results"] = "Filterergebnisse";
+
+$Definition["General Appearance"] = "Allgemeine Erscheinung";
+
+$Definition["Hello Home Page. Links are below:"] = "Hallo, Startseite. Die Links sind unten aufgeführt:";
+
+$Definition["Insert Article Here"] = "Artikel hier einfügen";
+
+$Definition["Knowledge"] = "Wissen";
+$Definition["Knowledge Base"] = "Wissensdatenbank";
+$Definition["Knowledge Base Item"] = "Element der Wissensdatenbank";
+$Definition["Knowledge Bases"] = "Wissensdatenbanken";
+
+$Definition["Load More"] = "Mehr laden";
+
+$Definition["More Articles"] = "Mehr Artikel";
+
+$Definition["Navigation Manager"] = "Navigationsmanager";
+$Definition["New Article"] = "Neuer Artikel";
+$Definition["New Category"] = "Neue Kategorie";
+$Definition["New Knowledge Base"] = "Neue Wissensdatenbank";
+$Definition["New Name: "] = "Neuer Name:";
+$Definition["No \"deleted\" knowledge bases found."] = "Keine „gelöschten“ Wissensdatenbanken gefunden.";
+$Definition["No Permission"] = "Keine Berechtigung";
+$Definition["No knowledge bases found."] = "Keine Wissensdatenbanken gefunden.";
+
+$Definition["On This Page"] = "Auf dieser Seite";
+$Definition["Organize Categories"] = "Kategorien sortieren";
+$Definition["Other Articles"] = "Weitere Artikel";
+$Definition["Other Languages"] = "Weitere Sprachen";
+
+$Definition["Page Location"] = "Position der Seite";
+$Definition["Page not found"] = "Seite nicht gefunden";
+$Definition["Please try again later."] = "Versuchen Sie es bitte später noch einmal.";
+$Definition["Publish"] = "Veröffentlichen";
+
+$Definition["Related Articles"] = "Verwandte Artikel";
+$Definition["Remove Article Link"] = "Artikel-Link entfernen";
+$Definition["Remove Link"] = "Link entfernen";
+$Definition["Rename"] = "Umbenennen";
+$Definition["Restore an Article"] = "Einen Artikel wiederherstellen";
+$Definition["Retry"] = "Erneut versuchen";
+$Definition["Revision History"] = "Überarbeitungschronik";
+$Definition["Revisions"] = "Überarbeitungen";
+
+$Definition["Saving draft..."] = "Entwurf wird gespeichert...";
+$Definition["Search Page"] = "Seite suchen";
+$Definition["Search Results"] = "Suchergebnisse";
+$Definition["Search in:"] = "Suchen in:";
+$Definition["Select a Category"] = "Kategorie auswählen";
+$Definition["Select where you want to place your Article."] = "Wählen Sie aus, wo Sie Ihren Artikel platzieren möchten.";
+$Definition["Set Page Location"] = "Position der Seite setzen";
+$Definition["Sub folder"] = "Unterordner";
+$Definition["Subcategories"] = "Unterkategorien";
+
+$Definition["Thanks for your feedback!"] = "Vielen Dank für Ihr Feedback!";
+$Definition["There are no categories here."] = "Hier gibt es keine Kategorien.";
+$Definition["There are no knowledge bases"] = "Keine Wissensdatenbanken vorhanden";
+$Definition["There was an error"] = "Es trat ein Fehler auf";
+$Definition["This article has been deleted."] = "Dieser Artikel wurde gelöscht.";
+$Definition["This category does not have any articles."] = "In dieser Kategorie gibt es keine Artikel.";
+$Definition["This is a destructive action. You will not be able to restore your draft."] = "Diese Aktion löscht Daten. Sie werden Ihren Entwurf nicht wiederherstellen können.";
+$Definition["This is a non-destructive action. You will be able to restore your article if you wish."] = "Bei dieser Aktion geht nichts verloren. Auf Wunsch können Sie Ihren Artikel wiederherstellen.";
+$Definition["This knowledge base does not have any articles."] = "In dieser Wissensdatenbank gibt es keine Artikel.";
+$Definition["This will create an article that is a copy of this discussion. The discussion will then be linked to the new article."] = "Dies erstellt einen Artikel, der eine Kopie dieser Diskussion ist. Die Diskussion wird dann mit dem neuen Artikel verlinkt.";
+$Definition["This will remove the link to the article, but will not remove the article itself."] = "Dies entfernt den Link zum Artikel; der Artikel selbst bleibt erhalten.";
+$Definition["Top Left"] = "Links oben";
+$Definition["Type your message."] = "Schreiben Sie Ihre Nachricht.";
+
+$Definition["Unknown User"] = "Unbekannter Nutzer";
+
+$Definition["View Article"] = "Artikel anzeigen";
+$Definition["View all articles from category: \"<0/>\"."] = "Alle Artikel aus der Kategorie anzeigen: \"<0/>\".";
+
+$Definition["Was this article helpful?"] = "War dieser Artikel hilfreich?";
+$Definition["Write Discussion"] = "Eine Diskussion schreiben";
+
+$Definition["You can see this message because you have special permissions."] = "Sie können diese Meldung sehen, weil Sie über spezielle Berechtigungen verfügen.";
+$Definition["You don't have permission to view this resource."] = "Sie haben keine Berechtigung, diese Ressource einzusehen.";
+$Definition["You need to <0>Sign In</0> to vote on this article"] = "Sie müssen sich <0>anmelden</0>, um über diesen Artikel abzustimmen";
