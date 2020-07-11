cfbd9c2091fb47d76613bfac463f2c1be9a1e456
Todd Burry <todd@vanillaforums.com>
2020-07-11 19:28:06 -0400

    Backup vf_hr before deleting

---
+++ vf_hr/site_kb.php
@@ -0,0 +1,13 @@
+<?php
+
+$Definition['A customized version of the knowledge base name as it should appear in URLs.'] = 'Prilagođena verzija naziva baze znanja onako kako bi se trebala pojaviti u URL-ovima.';
+$Definition['Add/Edit Knowledge Base'] = 'Dodaj/uredi bazu znanja';
+$Definition['Add a knowledge base to get started.'] = 'Dodajte bazu znanja za početak.';
+$Definition['Add Knowledge Base'] = 'Dodajte bazu znanja';
+$Definition['A description of the knowledge base. Displayed in the knowledge base picker.'] = 'Opis baze znanja. Prikazuje se u biraču baze znanja.';
+$Definition['Admin Links'] = 'Admin linkovi';
+$Definition['Alphabetically'] = 'Po abecednom redu';
+$Definition['Are you sure you want to delete this knowledge base?'] = 'Jeste li sigurni da želite izbrisati ovu bazu znanja?';
+$Definition['Are you sure you want to purge this knowledge base?'] = 'Jeste li sigurni da želite očistiti ovu bazu znanja?';
+$Definition['Are you sure you want to restore this knowledge base?'] = 'Jeste li sigurni da želite obnoviti ovu bazu znanja?';
+$Definition['A small image used to represent the knowledge base. Displayed in the knowledge base picker.'] = 'Mala slika koja se koristi za predstavljanje baze znanja. Prikazuje se u biraču baze znanja.';
