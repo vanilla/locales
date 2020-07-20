d568b7ba6c69568ddc968b1d2a1ad7504e91c930
Todd Burry <todd@vanillaforums.com>
2020-07-20 17:12:34 -0400

    Tweak the logo descriptions to add a disclaimer

--- tx-source/dash_text.php
+++ tx-source/dash_text.php
@@ -117,7 +117,8 @@ $Definition['Learn about custom routing.'] = 'Learn about custom routing.';
 $Definition['Learn how to completely change your forum\'s look and feel: upload your logo, set your homepage, choose a theme and customize it.'] = 'Learn how to completely change your forum\'s look and feel: upload your logo, set your homepage, choose a theme and customize it.';
 $Definition['Learn how to use the basic functionality of your forum.'] = 'Learn how to use the basic functionality of your forum.';
 $Definition['Locales allow you to support other languages on your site.'] = 'Locales allow you to support other languages on your site. Enable and disable locales you want to make available here.';
-$Definition['LogoDescription'] = 'The banner logo appears at the top of your site. Some themes may not display this logo.';
+$Definition['LogoDescription'] = 'The banner logo appears at the top of your site.';
+$Definition['LogoDisclaimer'] = 'Themes made with the theme editor and some custom themes don\'t use this setting.';
 
 $Definition['Make sure you click View Page'] = 'Make sure you click <a href="%s">View Page</a> to see what your categories page looks like after saving.';
 $Definition['Manage Routes'] = 'Manage Routes';
@@ -125,7 +126,7 @@ $Definition['Marking things as NOT spam will put them back in your forum.'] = 'M
 $Definition['Marking things as not spam will put them back in your forum.'] = 'Marking things as not spam will put them back in your forum.';
 $Definition['Messages can appear anywhere in your application.'] = 'Messages can appear anywhere in your application, and can be used to inform your users of news and events. Use this page to re-organize your messages by dragging them up or down.';
 $Definition['MobileLogoDescription'] =
-    'The mobile banner logo appears at the top of your site. Some themes may not display this logo.';
+    'The mobile banner logo appears at the top of your site.';
 $Definition['Mobile themes allow you to change the look and feel of your site on smaller devices.'] =
     'Mobile themes allow you to change the look and feel of your site on smaller devices.';
 
