417d9adeef14b02ad80a6869de51a5ad3089715d
Patrick Kelly <nervoustwit@users.noreply.github.com>
2020-08-26 15:09:12 -0400

    Update tx-source/dash_core.php
    Co-authored-by: Dani <31856281+dannimajdalani@users.noreply.github.com>

--- tx-source/dash_core.php
+++ tx-source/dash_core.php
@@ -718,7 +718,7 @@ $Definition['The homepage title is displayed on your home page.'] = 'The homepag
 $Definition['The homepage was saved successfully.'] = 'The homepage was saved successfully.';
 $Definition['The invitation was removed successfully.'] = 'The invitation was removed successfully.';
 $Definition['The invitation was sent successfully.'] = 'The invitation was sent successfully.';
-$Definition['The Key in the XML payload to designate %s'] = 'The Key in the XML payload to designate %s';
+$Definition['The Key in the XML payload to designate %s.'] = 'The Key in the XML payload to designate %s.';
 $Definition['Theme Editor'] = 'Theme Editor';
 $Definition['Theme Options'] = 'Theme Options';
 $Definition['Themes'] = 'Themes';
@@ -894,4 +894,3 @@ $Definition['Display on category page only.'] = 'Display on category page only. 
 $Definition['Use Masonry display'] = 'Use Masonry display';
 $Definition['Show an uploaded image on the Category page.'] = 'Each discussion will show an uploaded image on the Category page. This only applies to categories with "Discussions" as the "Display As."';
 $Definition['Upload Placeholder Image'] = 'Upload Placeholder Image';
-
