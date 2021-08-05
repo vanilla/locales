b9e72f12e8b4d2eae7154c2f26a2f9ac245d1e47
Richard Flynn <richardflynn@gmail.com>
2021-08-05 16:28:32 -0400

    Revert "Revert "fix translated html entities""
    This reverts commit e34d8afd1dcc4a5844418d49343350452effe699.

--- vf_de/definitions.php
+++ vf_de/definitions.php
@@ -5,7 +5,7 @@
   'Name' => 'Deutsch / German',
   'EnName' => 'German',
   'Description' => 'Official German language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/de/">here</a>.',
-  'Version' => '2021.08.04p1343',
+  'Version' => '2021.08.05p1616',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/de/',
   'License' => 'CC BY-SA 4.0',
