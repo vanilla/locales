e5c80be0c1917ce153cdf2e9d1bd2626e7812bd5
Mike Masson <49449010+mike-masson@users.noreply.github.com>
2022-10-12 17:59:47 -0400

    APPENG-19113 oct 2022 translations (#640)

--- vf_gd/definitions.php
+++ vf_gd/definitions.php
@@ -5,7 +5,7 @@
   'Name' => 'Gàidhlig / Scottish Gaelic',
   'EnName' => 'Scottish Gaelic',
   'Description' => 'Official Scottish Gaelic language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/gd/">here</a>.',
-  'Version' => '2022.09.22p1402',
+  'Version' => '2022.10.12p1655',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/gd/',
   'License' => 'CC BY-SA 4.0',
