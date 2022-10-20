d9579b9dd01609d2b983ad7ba0aa755db7601bb2
LafcadioHL <lhess@higherlogic.com>
2022-10-20 14:38:18 -0400

    APPENG-19386 and APPENG-19317 String addition
    Txsync Pull
    Adding strings requested from APPENG-19386 and APPENG-19317

--- vf_en_GB/definitions.php
+++ vf_en_GB/definitions.php
@@ -5,7 +5,7 @@
   'Name' => 'English (United Kingdom)',
   'EnName' => 'English (United Kingdom)',
   'Description' => 'Official English (United Kingdom) language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/en_GB/">here</a>. This locale is for British English. If you want to use the default American English you don\'t need to enable a specific locale pack.',
-  'Version' => '2022.10.12p1655',
+  'Version' => '2022.10.20p1427',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/en_GB/',
   'License' => 'CC BY-SA 4.0',
