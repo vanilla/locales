fff49acdce12fdd5fc56810c2337dbacf0300d9d
Lafcadio Hess <92742661+LafcadioHL@users.noreply.github.com>
2023-05-18 12:11:01 -0400

    APPENG-23729 Adding additional translations strings for search filter

--- vf_tr/missing.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$Definition['Account & Privacy Settings'] = 'Account &amp; Privacy Settings';
-
-$Definition['CategoriesNoFollowed'] = 'No categories followed';
-
-$Definition['You can specify an allow list of trusted domains.'] = 'You can specify a list of trusted domains that are safe for redirects & embedding.';
