46dfddddc6a4a9011aeeed0b0ee3960e80d738bf
Lafcadio Hess <92742661+LafcadioHL@users.noreply.github.com>
2023-05-26 14:24:47 -0400

    VNLA-4312 Addint BulkImport locale strings

--- tx-source/site_core.php
+++ tx-source/site_core.php
@@ -2236,3 +2236,9 @@ $Definition['Skip to content'] = 'Skip to content';
 
 // KB Articles
 $Definition["Read More"] = "Read More";
+
+//BulkImporter
+$Definition["Hello,\n\nAn account has been created for you at the %s forum.\n\n"] = "Hello,\n\nAn account has been created for you at the %s forum.\n\n";
+$Definition["To activate your account, please follow this link:\n%s\n\n"] = "To activate your account, please follow this link:\n%s\n\n";
+$Definition["Please contact us if you have questions regarding this email.\n\nSincerely,\n%s"] = "Please contact us if you have questions regarding this email.\n\nSincerely,\n%s";
+
