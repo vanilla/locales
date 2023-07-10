ea5ee4a879eb6eb5530841a020ee51c04005df12
John McKenzie <82835916+John-McKenzie@users.noreply.github.com>
2023-07-10 14:37:30 -0700

    Machine translations completed July 2023 (#704)

--- vf_ar/missing.php
+++ vf_ar/missing.php
@@ -2,5 +2,3 @@
 
 $Definition['PreferenceGroupNewDiscussionEmail'] = 'Notify me of new discussions in groups I am a member of.';
 $Definition['PreferenceGroupNewDiscussionPopup'] = 'Notify me of new discussions in groups I am a member of.';
-
-$Definition['when people have answered your question'] = 'when there has been a follow-up to your answered questions';
