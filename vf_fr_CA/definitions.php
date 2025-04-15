ca37098a070cce862e66fcdec16b4d80fb24b13b
Richard Flynn <rflynn@higherlogic.com>
2025-04-15 16:59:10 -0400

    fix remaining bad translation

--- vf_fr_CA/definitions.php
+++ vf_fr_CA/definitions.php
@@ -5,7 +5,7 @@
   'Name' => 'Français (Canada) / French (Canada)',
   'EnName' => 'French (Canada)',
   'Description' => 'Official French (Canada) language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/fr_CA/">here</a>.',
-  'Version' => '2025.04.15-04:001655',
+  'Version' => '2025.04.15-04:001658',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/fr_CA/',
   'License' => 'CC BY-SA 4.0',
