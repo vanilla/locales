9d145d72fb66bd0bda662f0d1a22e928dcd72e94
David Barbier <dbarbier@higherlogic.com>
2025-06-02 14:58:25 -0400

    Updated wording according to PO feedback

--- tx-source/site_core.php
+++ tx-source/site_core.php
@@ -934,7 +934,7 @@ $Definition['How many levels deep should we start folding up quote trees?'] = 'H
 $Definition['I agree to the <a id="TermsOfService" class="Popup" target="terms" href="%s">terms of service</a>'] = 'I agree to the <a id="TermsOfService" class="Popup" target="terms" href="%s">terms of service</a>';
 $Definition['Icon'] = 'Icon';
 $Definition["Icon for: "] = "Icon for: ";
-$Definition["Ideas only support upvotes and downvotes. Converting this post to an Idea will remove all unsupported reactions."] = "Ideas only support upvotes and downvotes. Converting this post to an Idea will remove all unsupported reactions.";
+$Definition["You're converting this post to a type that only allows upvotes and downvotes. Unsupported reactions will be removed."] = "You're converting this post to a type that only allows upvotes and downvotes. Unsupported reactions will be removed.";
 $Definition['I confirm that I have received consent to join this community.'] = 'Since I\'m under %d years old, I confirm that I have received consent to join this community.';
 $Definition["I'm Interested"] = "I'm Interested";
 $Definition['Idea'] = 'Idea';
