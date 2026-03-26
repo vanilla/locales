ffa9b595c53c631515af4c13eaec1a3fcd4aa234
Richard Flynn <rflynn@higherlogic.com>
2026-03-26 08:05:08 -0400

    Machine Translation Sync March 26, 2026

--- vf_es_MX/definitions.php
+++ vf_es_MX/definitions.php
@@ -5,7 +5,7 @@
   'Name' => 'Español (México) / Spanish (Mexico)',
   'EnName' => 'Spanish (Mexico)',
   'Description' => 'Official Spanish (Mexico) language translations for Vanilla. Help contribute to this translation by going to its translation site <a href="https://www.transifex.com/projects/p/vanilla/language/es_MX/">here</a>.',
-  'Version' => '2026.03.24-04:000954',
+  'Version' => '2026.03.25-04:001325',
   'Author' => 'Vanilla Community',
   'AuthorUrl' => 'https://www.transifex.com/projects/p/vanilla/language/es_MX/',
   'License' => 'CC BY-SA 4.0',
